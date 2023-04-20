76b7287f5454571055d9aba531e01aec944127cf
Emerson Trindade <trinddade29@gmail.com>
2023-04-20 14:44:53 -0300

    aplicating react hook form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,34 @@
-import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SeacrhFormContiner } from "./styles";
+import { MagnifyingGlass } from "phosphor-react";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import * as z from 'zod';
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  return (
-    <SeacrhFormContiner>
-      <input type="text" placeholder="Busque por transações" />
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
 
-      <button type="submit">
-        <MagnifyingGlass size={20}/>
+  function handleSearchTransactions(data: SearchFormInputs) {
+    console.log(data)
+  }
+
+  return (
+    <SeacrhFormContiner onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
         Buscar
       </button>
     </SeacrhFormContiner>
--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -35,8 +35,14 @@ export const SeacrhFormContiner = styled.form`
       color: ${theme['green-300']};
       font-weight: bold;
       border-radius: 6px;
+      cursor: pointer;
 
-      &:hover {
+      &:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${theme['green-500']};
         border-color: 1px solid ${theme['green-500']};
         color: ${theme.white};
